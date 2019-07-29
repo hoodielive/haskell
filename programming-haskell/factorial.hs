3880f2a2e1d96a4cf81cc6ace77d431af6534ab5
hoodielive <hoodielive@gmail.com>
2019-07-29 19:03:12 -0400

    um..

--- programming-haskell/factorial.hs
+++ programming-haskell/factorial.hs
@@ -1,6 +1,8 @@
-fac :: (Integral a) => a -> a -- Integral is the class of integral num types 
+fac :: (Integral a) => a -> a -- Integral --> for ex: int/integer
 fac 0 = 1
 fac n = n * fac (n - 1)
 
 main :: IO ()
 main = print $ fac 5
+
+
