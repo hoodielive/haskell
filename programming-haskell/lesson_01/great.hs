b521b94f9203a43fcd9f90de1f89c54605662cd9
hoodielive <hoodielive@gmail.com>
2019-07-31 23:25:43 -0400

    haskella

--- programming-haskell/lesson_01/great.hs
+++ programming-haskell/lesson_01/great.hs
@@ -7,3 +7,9 @@ doubleSmallNumber x = if x > 100
                         else x*2
 
 doubleSmallNumberAddOne x = (if x > 100 then x else x * 2) + 1
+
+-- ' is a valid character in functions 
+
+osa'meji = "This is odu ifa and very powerful it is!"
+
+let lostNumbers = [4,8,15,16,23,42]
