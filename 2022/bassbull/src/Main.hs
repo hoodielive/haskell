a08154411339eb4754c05d35283419006a9b416d
hoodielive <hoodielive@gmail.com>
2022-01-14 23:18:22 -0500

    No more haskell for tonight.

--- 2022/bassbull/src/Main.hs
+++ 2022/bassbull/src/Main.hs
@@ -1,5 +1,18 @@
 module Main where
 
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+
+-- from casava
+import Data.Csv
+
+-- a simple type alias for data
+type BaseballStats = (BL.ByteString, Int, BL.ByteString, Int)
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  csvData <- BL.readFile "battling.csv"
+  let v = decode NoHeader csvData :: Either String (V.Vector BaseballStats)
+  let summed = fmap (V.foldr summer 0) v
+  putStrLn $ "Total atBats was: " ++ (show summed)
+  where summer (name, year, team, atBats) n = n + atBats
